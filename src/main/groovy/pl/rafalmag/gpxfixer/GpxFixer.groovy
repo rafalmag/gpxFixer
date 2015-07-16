29f57519fe54071a0f9a9b9f440311e5ac0e20c0
Rafal Magda <rafalmag@gmail.com>
2015-07-16 23:34:55 +0200

    groovy way

--- src/main/groovy/pl/rafalmag/gpxfixer/GpxFixer.groovy
+++ src/main/groovy/pl/rafalmag/gpxfixer/GpxFixer.groovy
@@ -6,9 +6,9 @@ import groovy.xml.XmlUtil
 
 import java.text.SimpleDateFormat
 
-public class GpxFixer {
+class GpxFixer {
     // eg. 1969-12-31T17:00:00Z
-    static final def DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    static def DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
 
     public static void main(String[] args) {
         def is = new FileInputStream(args[0])
@@ -22,36 +22,32 @@ public class GpxFixer {
         println("done")
     }
 
-    private def static GPathResult getRootFromStream(InputStream is) {
-        try {
-            new XmlSlurper().parse(is)
-        } finally {
-            try { is.close() } catch (IOException ignore) {}
-        }
+    def static GPathResult getRootFromStream(InputStream is) {
+        is.withCloseable { new XmlSlurper().parse(it) }
     }
 
-    private final GPathResult root
-    private final Date startTime
-    private final Date xmlStartTime
+    final GPathResult root
+    final Date startTime
+    final Date xmlStartTime
 
-    public GpxFixer(GPathResult root, Date startTime) {
+    GpxFixer(GPathResult root, Date startTime) {
         this.root = root
         this.startTime = startTime
         this.xmlStartTime = DATE_FORMAT.parse(root.trk.time.toString())
         println("Start time $xmlStartTime")
     }
 
-    def fixAndSave(Writer writer) {
+    void fixAndSave(Writer writer) {
         fixMasterTime()
         fixPointsTimes()
         save(writer)
     }
 
-    private def void fixMasterTime() {
+    void fixMasterTime() {
         root.trk.time = DATE_FORMAT.format(startTime)
     }
 
-    private def void fixPointsTimes() {
+    void fixPointsTimes() {
         long offsetMs = calculateOffsetMs()
         def times = root.trk.trkseg.trkpt.time
         println("First original time: ${times[0]}")
@@ -64,31 +60,24 @@ public class GpxFixer {
         println("First original time converted: ${times[0]}")
     }
 
-    private def long calculateOffsetMs() {
-        long offsetMs = startTime.getTime() - xmlStartTime.getTime()
-        offsetMs
+    long calculateOffsetMs() {
+        startTime.getTime() - xmlStartTime.getTime()
     }
 
-    private String convertToString(GPathResult doc) {
+    String convertToString(GPathResult doc) {
         def defaultNamespace = doc.lookupNamespace('')
-
         if (defaultNamespace) {
             def docWithNamespace = {
                 mkp.declareNamespace("": defaultNamespace)
                 out << doc
             }
-            return new StreamingMarkupBuilder().bind(docWithNamespace)
+            new StreamingMarkupBuilder().bind(docWithNamespace)
         } else {
-            return XmlUtil.serialize(doc as GPathResult)
+            XmlUtil.serialize(doc as GPathResult)
         }
     }
 
-    private def void save(Writer writer) {
-        try {
-            writer.write(convertToString(root))
-        } finally {
-            writer.close()
-        }
+    void save(Writer writer) {
+        writer.withCloseable { it.write(convertToString(root)) }
     }
-
 }
